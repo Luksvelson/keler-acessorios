95a4ec0d2445062233f38084c6a98b01448af0dc
Luksvelson <lukasrj84@gmail.com>
2019-12-28 14:59:02 -0300

    alteracao no produto.component.ts

--- src/app/produto/produto.component.ts
+++ src/app/produto/produto.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ProdutoFormComponent } from './produto-form/produto-form.component';
+import { ProdutoService } from '../services/produto.service';
+import { Produto } from '../models/produto';
 
 @Component({
   selector: 'produto',
@@ -9,22 +11,50 @@ import { ProdutoFormComponent } from './produto-form/produto-form.component';
 })
 export class ProdutoComponent implements OnInit {
 
-  constructor(private modalService: NgbModal) { }
+  constructor(
+    private modalService: NgbModal,
+    private produtoService: ProdutoService,
+  ) { }
 
   ngOnInit() {
+    this.mostrarProdutos();
   }
-
+  
   addProduto() {
     const modal = this.modalService.open(ProdutoFormComponent);
     modal.result.then(
       this.handleModalProdutoForm.bind(this),
       this.handleModalProdutoForm.bind(this)
-    )
+      )
     }
-
-  handleModalProdutoForm(response) {
-    alert('Janela Fechada');
-  }
     
+    handleModalProdutoForm(response) {
+      
+    }
+    
+    produtos : Produto[] = [];
+    
+    mostrarProdutos() {
+      this.produtoService.getProdutos().subscribe(response => {
+      this.produtos = [];
+      response.docs.forEach(value => {
+        const data = value.data();
+        const produto : Produto = {
+          codigoProduto: data.codigoProduto,
+          categoria: data.categoria,
+          descricao: data.descricao
+        };
+        this.produtos.push(produto);
+      });
+    });
+  }
+
+  // editarProduto() {
+  //   this.produtoService.editarProdutos()
+  // }
+
+  // deletarProduto() {
+
+  // }
 
 }
